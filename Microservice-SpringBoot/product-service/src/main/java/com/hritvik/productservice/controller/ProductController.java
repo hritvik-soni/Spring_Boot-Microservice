fb68cb51ddff1ac39cc81df9b04a5a583dc22d84
Neha Kumari <neha.kumari1vns@gmail.com>
2023-09-15 18:21:57 +0530

    add file

--- Microservice-SpringBoot/product-service/src/main/java/com/hritvik/productservice/controller/ProductController.java
+++ Microservice-SpringBoot/product-service/src/main/java/com/hritvik/productservice/controller/ProductController.java
@@ -1,4 +1,15 @@
 package com.hritvik.productservice.controller;
 
+import com.hritvik.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+    @Autowired
+    ProductService productService;
+
+
 }
--- Microservice-SpringBoot/product-service/src/main/java/com/hritvik/productservice/model/Product.java
+++ Microservice-SpringBoot/product-service/src/main/java/com/hritvik/productservice/model/Product.java
@@ -1,6 +1,29 @@
 package com.hritvik.productservice.model;
 
+import com.hritvik.productservice.model.enums.ProductCategory;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "product_table")
 public class Product {
-    long productId;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String productId;
+    private String productName;
+    private  String productDescription;
+    private ProductCategory productCategory;
+    private Long productPrice;
+    private Integer productQuantity;
+    private String SqCode;
 
 }
