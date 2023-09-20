ab9aebc4825ebf74e6b0b459110a30d79679931d
Neha Kumari <neha.kumari1vns@gmail.com>
2023-09-20 19:36:21 +0530

    add files

---
+++ Microservice-SB-Ticket-Booking/bus-service/src/main/java/com/learning/busservice/model/dto/BusRequestOutput.java
@@ -0,0 +1,22 @@
+package com.learning.busservice.model.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BusRequestOutput {
+    private String busCityFrom;
+    private String busCityTo;
+    private String busName;
+    private Integer busTicketPrice;
+    private LocalTime busDepartureTime;
+    private LocalTime busArrivalTime;
+}
