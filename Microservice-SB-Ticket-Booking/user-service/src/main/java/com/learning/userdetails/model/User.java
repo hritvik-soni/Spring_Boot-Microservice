e0c6918c3c3ec1fc5408f3fe129bcffdf05e0821
Hritvik Soni <hritviksoni768@gmail.com>
2023-09-21 11:22:05 +0530

    added

--- Microservice-SB-Ticket-Booking/user-service/src/main/java/com/learning/userdetails/model/User.java
+++ Microservice-SB-Ticket-Booking/user-service/src/main/java/com/learning/userdetails/model/User.java
@@ -21,7 +21,7 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Integer userId;
     @NotBlank
     @NotNull
     private String userName;
--- Microservice-SB-Ticket-Booking/user-service/src/main/java/com/learning/userdetails/repository/IUserRepo.java
+++ Microservice-SB-Ticket-Booking/user-service/src/main/java/com/learning/userdetails/repository/IUserRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IUserRepo extends JpaRepository<User,Long> {
+public interface IUserRepo extends JpaRepository<User,Integer> {
     User findByUserEmail(String email);
 }
